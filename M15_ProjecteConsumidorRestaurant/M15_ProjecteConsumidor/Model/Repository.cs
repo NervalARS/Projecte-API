90ebbb576411b547cd3c563474ce9c452924f878
NervalARS <56166644+NervalARS@users.noreply.github.com>
2020-12-03 19:59:39 +0100

    Cambio de proyecto

---
+++ M15_ProjecteConsumidorRestaurant/M15_ProjecteConsumidor/Model/Repository.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace M15_ProjecteConsumidor.Model
+{
+    class Repository
+    {
+        private static string ws1 = "https://api.documenu.com/v2/restaurant/4072702673999819?key=YOUR_API_KEY_GOES_HERE/";
+        public static List<Song> GetSongsForArtist(string artistId)
+        {
+            List<Song> lc = (List<Song>)MakeRequest(string.Concat(ws1, "artists/{artistId}/songs"), null, "GET", "application/json", typeof(List<Song>));
+            return lc;
+        }
+
+        public static Artist GetArtist(string artistId)
+        {
+            Artist a = (Artist)MakeRequest(string.Concat(ws1, "artists/{artistId}"), null, "GET", "application/json", typeof(Artist));
+            return a;
+        }
+        public static Song GetSong(string songId)
+        {
+            Song s = (Song)MakeRequest(string.Concat(ws1, "songs/{songId}"), null, "GET", "application/json", typeof(Song));
+            return s;
+        }
+
+        public static object MakeRequest(string requestUrl, object JSONRequest, string JSONmethod, string JSONContentType, Type JSONResponseType)
+        //  requestUrl: Url completa del Web Service, amb l'opció sol·licitada
+        //  JSONrequest: objecte que se li passa en el body 
+        //  JSONmethod: "GET"/"POST"/"PUT"/"DELETE"
+        //  JSONContentType: "application/json" en els casos que el Web Service torni objectes
+        //  JSONRensponseType:  tipus d'objecte que torna el Web Service (typeof(tipus))
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest; //WebRequest WR = WebRequest.Create(requestUrl);   
+                string sb = JsonConvert.SerializeObject(JSONRequest);
+                request.Method = JSONmethod;  // "GET"/"POST"/"PUT"/"DELETE";  
+
+                if (JSONmethod != "GET")
+                {
+                    request.ContentType = JSONContentType; // "application/json";   
+                    Byte[] bt = Encoding.UTF8.GetBytes(sb);
+                    Stream st = request.GetRequestStream();
+                    st.Write(bt, 0, bt.Length);
+                    st.Close();
+                }
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        throw new Exception(String.Format("Server error (HTTP {0}: {1}).", response.StatusCode, response.StatusDescription));
+
+                    Stream stream1 = response.GetResponseStream();
+                    StreamReader sr = new StreamReader(stream1);
+                    string strsb = sr.ReadToEnd();
+                    object objResponse = JsonConvert.DeserializeObject(strsb, JSONResponseType);
+                    return objResponse;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
