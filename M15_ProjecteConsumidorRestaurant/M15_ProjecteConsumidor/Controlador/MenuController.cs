35d229e4372c60a8397168b5b049658bf8167770
Kluque <kevin10082001@gmail.com>
2021-01-15 21:06:25 +0100

    No te borres porfavor >V

--- M15_ProjecteConsumidorRestaurant/M15_ProjecteConsumidor/Controlador/MenuController.cs
+++ M15_ProjecteConsumidorRestaurant/M15_ProjecteConsumidor/Controlador/MenuController.cs
@@ -13,12 +13,46 @@ namespace M15_ProjecteConsumidor.Controlador
     {
         VMenu VM = new VMenu();
         MMenu MEN = new Model.MMenu();
-        public MenuController(MMenu men)
+        public MenuController(List<MenuItem> s)
         {
+            carga(s);
             this.MEN = men;
+            InitListeners();
             VM.ShowDialog();
         }
 
+         private void carga(List<MenuItem> ss)
+        {
+            VM.dataGridView1.DataSource = ss;
+        }
 
+        private void InitListeners()
+        {
+            VM.BT_EXIT += exitMenusVist;
+            VM.dataGridView1.SelectionChanged += SelectedItem;
+        }
+
+        private void exitMenusVist(Object sender, EventArgs e)
+        {
+            VM.Close();
+        }
+
+        private void SelectedItem(Object sender, EventArgs e)
+        {
+            /*
+             string Name
+             string Description
+             double Price*/
+
+            if (!VM.dataGridView1.SelectedRows.Count > 0)
+            {
+                MenuItem MM = VM.dataGridView1.SelectedRows;
+                VM.label1.Text(MM.Name);
+                /*string Title =VM.dataGridView1.SelectedRows[0].Cells[0];
+                string Descripcion = VM.dataGridView1.SelectedRows[0].Cells[1];
+                double Price = VM.dataGridView1.SelectedRows[0].Cells[3];
+            */
+           }
+        }
     }
 }
--- M15_ProjecteConsumidorRestaurant/M15_ProjecteConsumidor/Vista/VMenu.Designer.cs
+++ M15_ProjecteConsumidorRestaurant/M15_ProjecteConsumidor/Vista/VMenu.Designer.cs
@@ -38,8 +38,6 @@ namespace M15_ProjecteConsumidor.Vista
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -52,10 +50,10 @@ namespace M15_ProjecteConsumidor.Vista
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(64)))), ((int)(((byte)(150)))));
             this.panel1.Controls.Add(this.BT_EXIT);
             this.panel1.Controls.Add(this.MEN_Nom);
-            this.panel1.Location = new System.Drawing.Point(-2, -2);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
             this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1080, 82);
+            this.panel1.Size = new System.Drawing.Size(720, 53);
             this.panel1.TabIndex = 2;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -70,10 +68,9 @@ namespace M15_ProjecteConsumidor.Vista
             this.BT_EXIT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BT_EXIT.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
             this.BT_EXIT.ForeColor = System.Drawing.Color.White;
-            this.BT_EXIT.Location = new System.Drawing.Point(732, 14);
-            this.BT_EXIT.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BT_EXIT.Location = new System.Drawing.Point(488, 9);
             this.BT_EXIT.Name = "BT_EXIT";
-            this.BT_EXIT.Size = new System.Drawing.Size(331, 55);
+            this.BT_EXIT.Size = new System.Drawing.Size(221, 36);
             this.BT_EXIT.TabIndex = 48;
             this.BT_EXIT.Text = "Volver a Inicio";
             this.BT_EXIT.UseVisualStyleBackColor = false;
@@ -83,9 +80,10 @@ namespace M15_ProjecteConsumidor.Vista
             this.MEN_Nom.AutoSize = true;
             this.MEN_Nom.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MEN_Nom.ForeColor = System.Drawing.Color.White;
-            this.MEN_Nom.Location = new System.Drawing.Point(19, 23);
+            this.MEN_Nom.Location = new System.Drawing.Point(13, 15);
+            this.MEN_Nom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.MEN_Nom.Name = "MEN_Nom";
-            this.MEN_Nom.Size = new System.Drawing.Size(139, 46);
+            this.MEN_Nom.Size = new System.Drawing.Size(95, 31);
             this.MEN_Nom.TabIndex = 0;
             this.MEN_Nom.Text = "Menus";
             this.MEN_Nom.Click += new System.EventHandler(this.MEN_Nom_Click);
@@ -94,11 +92,12 @@ namespace M15_ProjecteConsumidor.Vista
             // 
             this.RES_Title1.AutoSize = true;
             this.RES_Title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RES_Title1.Location = new System.Drawing.Point(20, 20);
+            this.RES_Title1.Location = new System.Drawing.Point(13, 13);
+            this.RES_Title1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.RES_Title1.Name = "RES_Title1";
-            this.RES_Title1.Size = new System.Drawing.Size(290, 36);
+            this.RES_Title1.Size = new System.Drawing.Size(195, 25);
             this.RES_Title1.TabIndex = 37;
-            this.RES_Title1.Text = "Secciones del menu:";
+            this.RES_Title1.Text = "Elementos del menu:";
             this.RES_Title1.Click += new System.EventHandler(this.RES_Title1_Click);
             // 
             // panel3
@@ -107,10 +106,10 @@ namespace M15_ProjecteConsumidor.Vista
             this.panel3.Controls.Add(this.panel2);
             this.panel3.Controls.Add(this.dataGridView1);
             this.panel3.Controls.Add(this.RES_Title1);
-            this.panel3.Location = new System.Drawing.Point(-2, 80);
+            this.panel3.Location = new System.Drawing.Point(-1, 52);
             this.panel3.Margin = new System.Windows.Forms.Padding(0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1085, 705);
+            this.panel3.Size = new System.Drawing.Size(723, 458);
             this.panel3.TabIndex = 14;
             this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             // 
@@ -120,12 +119,9 @@ namespace M15_ProjecteConsumidor.Vista
             this.panel2.Controls.Add(this.label4);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.comboBox1);
-            this.panel2.Location = new System.Drawing.Point(27, 357);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Location = new System.Drawing.Point(18, 232);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1017, 298);
+            this.panel2.Size = new System.Drawing.Size(678, 194);
             this.panel2.TabIndex = 45;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
@@ -134,10 +130,11 @@ namespace M15_ProjecteConsumidor.Vista
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("MV Boli", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(626, 80);
+            this.label4.Location = new System.Drawing.Point(417, 52);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label4.Size = new System.Drawing.Size(373, 133);
+            this.label4.Size = new System.Drawing.Size(246, 87);
             this.label4.TabIndex = 47;
             this.label4.Text = "0.00€";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -147,9 +144,11 @@ namespace M15_ProjecteConsumidor.Vista
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(51, 137);
+            this.label3.Location = new System.Drawing.Point(34, 52);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.MaximumSize = new System.Drawing.Size(200, 500);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(180, 36);
+            this.label3.Size = new System.Drawing.Size(120, 25);
             this.label3.TabIndex = 46;
             this.label3.Text = "Descripción:";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -159,56 +158,32 @@ namespace M15_ProjecteConsumidor.Vista
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(51, 80);
+            this.label1.Location = new System.Drawing.Point(34, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(128, 36);
+            this.label1.Size = new System.Drawing.Size(87, 25);
             this.label1.TabIndex = 45;
             this.label1.Text = "Nombre:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(24, 14);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(260, 36);
-            this.label2.TabIndex = 42;
-            this.label2.Text = "Item de la seccion:";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.ItemHeight = 20;
-            this.comboBox1.Location = new System.Drawing.Point(285, 18);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(328, 28);
-            this.comboBox1.TabIndex = 44;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(27, 78);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Location = new System.Drawing.Point(18, 51);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.Size = new System.Drawing.Size(1017, 231);
+            this.dataGridView1.Size = new System.Drawing.Size(678, 150);
             this.dataGridView1.TabIndex = 39;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // VMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1074, 776);
+            this.ClientSize = new System.Drawing.Size(716, 504);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "VMenu";
             this.Text = "Menu";
             this.Load += new System.EventHandler(this.VMenu_Load);
@@ -230,8 +205,6 @@ namespace M15_ProjecteConsumidor.Vista
         public System.Windows.Forms.Label RES_Title1;
         public System.Windows.Forms.Panel panel3;
         public System.Windows.Forms.DataGridView dataGridView1;
-        public System.Windows.Forms.Label label2;
-        public System.Windows.Forms.ComboBox comboBox1;
         public System.Windows.Forms.Panel panel2;
         public System.Windows.Forms.Label label4;
         public System.Windows.Forms.Label label3;
